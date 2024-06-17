bbc9571dd44578c1a23b489b39fbdfef8e2ae395
demonuchy <demonuchy@gmail.com>
2024-06-17 12:37:48 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
+import React, { useState } from "react";
 import { MakeProduct } from "./components/productChain";
 
 function App() {
+  const [like, func] = useState(0);
+  console.log(like, func());
+
   return (
     <>
       <h1>Hello Word</h1>
       <MakeProduct />
+      <div>
+        <button>лайк +</button>
+        <button>лайк -</button>
+      </div>
     </>
   );
 }
